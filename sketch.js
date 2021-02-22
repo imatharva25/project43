21756b79b17a59db907690a916194f80269daab1
imatharva25 <73231948+imatharva25@users.noreply.github.com>
2021-02-22 15:56:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  hr= hour();
+  mn= minute();
+  sc= second();
+ angleMode(DEGREES);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+
+  scAngle= map(sc,0,60,0,360);
+  mnAngle= map(mn,0,60,0,360);
+  hrAngle= map(hr,0,60,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+  drawSprites();
+}
